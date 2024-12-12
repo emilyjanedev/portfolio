e7205e2c7e9b020294a5bcde2db1c6426dbce8ce
Emily Schur <ejschur@gmail.com>
2024-12-12 14:21:14 -0800

    Add project section animations

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,18 @@
 import { twMerge } from "tailwind-merge";
-import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef } from "react";
 
 const Card = ({
   className,
   children,
-}: PropsWithChildren<{ className?: string }>) => {
+  ...other
+}: ComponentPropsWithoutRef<"div">) => {
   return (
     <div
       className={twMerge(
-        "bg-gray-900/80 backdrop-blur rounded-3xl relative z-0 overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-10 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/30 after:pointer-events-none",
+        "bg-gray-950/90 backdrop-blur rounded-3xl relative z-0 overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-10 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/30 after:pointer-events-none",
         className
       )}
+      {...other}
     >
       {children}
     </div>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -16,10 +16,13 @@ export const ProjectsSection = () => {
           technologies I have been diving into."
         />
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, index) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky top-24"
+              style={{
+                top: `calc(96px + ${index * 40}px)`,
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
